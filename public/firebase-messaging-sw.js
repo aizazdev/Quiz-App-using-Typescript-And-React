9f237db37f53f8ee1a8e1d24bba367e47d914131
Aizaz Ahmad <65584802+aizazdev@users.noreply.github.com>
2021-06-17 02:20:34 -0700

    Install Libraries

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCILmDTG6ax0Ez7W1_j9YLWclVM_RPLv68",
+    authDomain: "notificationapp-9e0dd.firebaseapp.com",
+    databaseURL: "https://notificationapp-9e0dd.firebaseio.com",
+    projectId: "notificationapp-9e0dd",
+    storageBucket: "notificationapp-9e0dd.appspot.com",
+    messagingSenderId: "469601298387",
+    appId: "1:469601298387:web:05a09ba640ab9fbcbf3126"
+  };
